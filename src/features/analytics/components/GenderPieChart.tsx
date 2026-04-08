185ef0d02ea8dfbd27d206337eb441aa2127a682
Lüz Itachi Vndra <luz.vndra@proton.me>
2026-04-08 16:05:08 +0530

    updates to handle deprecation in pie chart
    still sticking to recharts 3.8.0 as 4.0.0 is not published yet

--- src/features/analytics/components/GenderPieChart.tsx
+++ src/features/analytics/components/GenderPieChart.tsx
@@ -1,4 +1,4 @@
-import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+import { PieChart, Pie, Tooltip, Legend } from "recharts";
 
 type GenderDistributionDatum = {
   name: "Male" | "Female" | "Other";
@@ -8,21 +8,22 @@ type GenderDistributionDatum = {
 const COLORS = ["#60A5FA", "#4ADE80", "#F472B6"];
 
 const GenderPieChart = ({ data }: { data: GenderDistributionDatum[] }) => {
+  const pieData = data.map((entry, index) => ({
+    ...entry,
+    fill: COLORS[index],
+  }));
+
   return (
     <PieChart width={400} height={300}>
       <Pie
-        data={data}
+        data={pieData}
         dataKey="value"
         nameKey="name"
         cx="50%"
         cy="50%"
         outerRadius={100}
         label
-      >
-        {data.map((_, index) => (
-          <Cell key={index} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
+      />
 
       <Tooltip
         contentStyle={{
