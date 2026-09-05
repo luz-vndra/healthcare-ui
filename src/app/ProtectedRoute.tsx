6b0bcd19be70866568bc44f39be50aabdab91b83
Lüz Itachi Vndra <luz.vndra@proton.me>
2026-09-05 17:59:19 +0530

    Update ProtectedRoute.tsx

--- src/app/ProtectedRoute.tsx
+++ src/app/ProtectedRoute.tsx
@@ -1,10 +1,17 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../auth/AuthContext";
+import { Spinner } from "react-bootstrap";
 
 const ProtectedRoute = ({ children }: any) => {
   const { user, loading } = useAuth();
 
-  if (loading) return <div>Loading...</div>;
+  if (loading)
+    return (
+      <div>
+        {" "}
+        <Spinner size="sm" /> Loading...
+      </div>
+    );
 
   if (!user) return <Navigate to="/login" />;
 
